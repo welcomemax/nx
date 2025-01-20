d1b6f5df0c1b626e5eabe8d24b2b166516210531
Nicholas Cunningham <ndcunningham@gmail.com>
2025-01-20 14:14:24 -0700

    feat(rspack): support multi-configuration mode in Rspack options

--- packages/rspack/src/utils/read-rspack-options.ts
+++ packages/rspack/src/utils/read-rspack-options.ts
@@ -5,53 +5,66 @@ import { readNxJsonFromDisk } from 'nx/src/devkit-internals';
 
 /**
  * Reads the Rspack options from a give Rspack configuration. The configuration can be:
- * 1. A standard config object
- * 2. A standard function that returns a config object
- * 3. A Nx-specific composable function that takes Nx context, rspack config, and returns the config object.
+ * 1. A single standard config object
+ * 2. A standard function that returns a config object (standard Rspack)
+ * 3. An array of standard config objects (multi-configuration mode)
+ * 4. A Nx-specific composable function that takes Nx context, rspack config, and returns the config object.
  *
  * @param rspackConfig
  */
 export async function readRspackOptions(
   rspackConfig: unknown
-): Promise<Configuration> {
-  let config: Configuration;
-  if (isNxRspackComposablePlugin(rspackConfig)) {
-    config = await rspackConfig(
-      {},
-      {
-        // These values are only used during build-time, so passing stubs here just to read out
-        // the returned config object.
-        options: {
-          root: workspaceRoot,
-          projectRoot: '',
-          sourceRoot: '',
-          outputFileName: '',
-          assets: [],
-          main: '',
-          tsConfig: '',
-          outputPath: '',
-          rspackConfig: '',
-          useTsconfigPaths: undefined,
+): Promise<Configuration[]> {
+  let configs: Configuration[] = [];
+
+  const resolveConfig = async (config: Configuration) => {
+    if (isNxRspackComposablePlugin(rspackConfig)) {
+      config = await rspackConfig(
+        {},
+        {
+          // These values are only used during build-time, so passing stubs here just to read out
+          // the returned config object.
+          options: {
+            root: workspaceRoot,
+            projectRoot: '',
+            sourceRoot: '',
+            outputFileName: '',
+            assets: [],
+            main: '',
+            tsConfig: '',
+            outputPath: '',
+            rspackConfig: '',
+            useTsconfigPaths: undefined,
+          },
+          context: {
+            root: workspaceRoot,
+            cwd: undefined,
+            isVerbose: false,
+            nxJsonConfiguration: readNxJsonFromDisk(workspaceRoot),
+            projectGraph: null,
+            projectsConfigurations: null,
+          },
+        }
+      );
+    } else if (typeof rspackConfig === 'function') {
+      config = await rspackConfig(
+        {
+          production: true, // we want the production build options
         },
-        context: {
-          root: workspaceRoot,
-          cwd: undefined,
-          isVerbose: false,
-          nxJsonConfiguration: readNxJsonFromDisk(workspaceRoot),
-          projectGraph: null,
-          projectsConfigurations: null,
-        },
-      }
-    );
-  } else if (typeof rspackConfig === 'function') {
-    config = await rspackConfig(
-      {
-        production: true, // we want the production build options
-      },
-      {}
-    );
+        {}
+      );
+    } else {
+      config = rspackConfig;
+    }
+    return config;
+  };
+
+  if (Array.isArray(rspackConfig)) {
+    for (const config of rspackConfig) {
+      configs.push(await resolveConfig(config));
+    }
   } else {
-    config = rspackConfig;
+    configs.push(await resolveConfig(rspackConfig));
   }
-  return config;
+  return configs;
 }
